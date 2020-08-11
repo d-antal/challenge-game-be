43f557031838d3ec313f71a603b531ae05852b69
AntalDutka <antaldutka@gmail.com>
2020-08-11 15:49:12 +0200

    Controllers

---
+++ src/main/java/com/lottoland/game/challenge/rockpaperscissors/controller/GameController.java
@@ -0,0 +1,32 @@
+package com.lottoland.game.challenge.rockpaperscissors.controller;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.lottoland.game.challenge.rockpaperscissors.model.Game;
+import com.lottoland.game.challenge.rockpaperscissors.service.GameServiceImpl;
+
+
+@CrossOrigin(origins = "http://localhost:4200")
+@RestController
+@RequestMapping("/api/v1")
+public class GameController {
+
+	@Autowired
+	private GameServiceImpl gameService;
+
+	@ResponseStatus(HttpStatus.CREATED)
+	@PostMapping(path = "/games")
+	public Game createGame(@Valid @RequestBody Game game) {	
+		return gameService.createGame(game);
+	}
+
+}
---
+++ src/main/java/com/lottoland/game/challenge/rockpaperscissors/controller/RoundController.java
@@ -0,0 +1,48 @@
+package com.lottoland.game.challenge.rockpaperscissors.controller;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.lottoland.game.challenge.rockpaperscissors.exception.ResourceNotFoundException;
+import com.lottoland.game.challenge.rockpaperscissors.model.Round;
+import com.lottoland.game.challenge.rockpaperscissors.model.RoundsTotal;
+import com.lottoland.game.challenge.rockpaperscissors.service.RoundServiceImpl;
+
+@CrossOrigin(origins = "http://localhost:4200")
+@RestController
+@RequestMapping("/api/v1/")
+public class RoundController {
+
+	@Autowired
+	private RoundServiceImpl roundService;
+
+	@GetMapping(path = "/games/{gameId}/rounds")
+	public List<Round> getRoundsByGame(@PathVariable(value = "gameId") Long gameId) {
+		return roundService.getRounds(gameId);
+	}
+
+	@ResponseStatus(HttpStatus.CREATED)
+	@PostMapping(path = "/games/{gameId}/rounds")
+	public Round createRound(@PathVariable(value = "gameId") Long gameId, @Valid @RequestBody Round round)
+			throws ResourceNotFoundException {
+		return roundService.createRound(round, gameId);
+	}
+
+	@GetMapping(path = "/games/rounds/total")
+	public RoundsTotal getRoundsTotal() {
+		return roundService.getRoundsTotal();
+	}
+
+}
